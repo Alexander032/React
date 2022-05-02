726bb489c9e1bf7d9d64e8f897a8bb2a90bdf4a9
Александр <wastem@mail.ru>
2022-05-02 18:51:15 +0300

    lesson-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
-import Text from "./Text";
+import {useEffect, useState} from "react";
+
 
 function App() {
-    const text = "будет интересно )";
+    const [messageList, setMessageList] = useState([]);
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const addMessage= () =>{
+      let random = Math.random()
+      setMessageList([...messageList, {
+        title: title, author: author, id:random
+      }])
+    };
+
+    useEffect( () =>{
+        setTimeout(()=>{
+          if (messageList.length > 0) {
+            alert('Сообщение отправлено')
+          }
+        }, 2000)
+    }, [messageList])
 
   return (
-    <div className="App">
-     <h1>Начинаем изучение React<Text Message ={text}/></h1>
+    <div  class = 'mesList container'>
+      <input value={title} onChange={(event)=> setTitle(event.target.value)}/>
+        <input value={author} onChange={(event) => setAuthor(event.target.value)}/>
+          <button style={{ color: "red", marginLeft: '20px' }} onClick={addMessage}>Отправить сообщение</button>
+        {messageList.map((item)=>{
+          return(
+            <div key={item.id} class='mesList1'>
+              <div style={{ color: 'bisque'}}>
+                {item.title}
+              </div>
+              <div style={{ color: 'black'}}>
+                {item.author}
+              </div>
+            </div>
+        )
+      })}
     </div>
   );
 }
